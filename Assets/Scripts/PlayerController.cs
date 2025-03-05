67161894e0dd124a8007bc89044fac329b8525c8
Posihe <kspill0708@naver.com>
2025-03-05 20:25:16 +0900

    2025-03-05

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     [Range(1, 10)]
     int speed;
+    [SerializeField]
+    [Range(1, 10)]
+    int jumpPower;
+    bool isGround;
+    [SerializeField]
+    int jumpCount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        isGround = true;
+        jumpCount = 1;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+        Jump();
+
+    }
+    private void Move()
     {
         float xinput = Input.GetAxis("Horizontal");
         float zinput = Input.GetAxis("Vertical");
@@ -23,4 +37,31 @@ public class PlayerController : MonoBehaviour
         Vector3 a = new Vector3(xspeed, 0, zspeed);
         rb.linearVelocity = a;
     }
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&&jumpCount>0)
+        {
+
+
+            
+            rb.AddForce(Vector3.up*jumpPower, ForceMode.Impulse); 
+            isGround = false; 
+            jumpCount--; 
+           
+
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround=true;
+            jumpCount = 1;
+
+
+        }
+    }
+  
 }
